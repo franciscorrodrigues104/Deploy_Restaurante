34d415b57f2425b5adcb1a938254c78c85a2c165
Francisco Ribeiro Rodrigues <francisco.rr104@gmail.com>
2026-08-03 14:17:15 +0100

    codigo

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// reveal-on-scroll
+  const revealEls = document.querySelectorAll('.reveal');
+  const io = new IntersectionObserver((entries)=>{
+    entries.forEach(e=>{
+      if(e.isIntersecting){ e.target.classList.add('in'); io.unobserve(e.target); }
+    });
+  }, { threshold: 0.12 });
+  revealEls.forEach(el=>io.observe(el));
